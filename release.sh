af4e6b44ce0f4e94736f7b7c1a6f9ad2069ad7e2
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2024-01-04 14:22:57 +0100

    build: allow negative phase flags in release script

--- release.sh
+++ release.sh
@@ -15,11 +15,17 @@
 #     - Notify: Post a message on Discord.
 #     - Summary: Update the main README of the repository.
 #
-#   By default, all phases are enabled, but when one is specified via flag, then
-#   they all become opt-in. By default, only text documentation is included in
-#   the release, but it can be controlled either by defining a ".release" file
-#   in the resource that overrides the defaults, or via flags; flags have the
-#   highest priority; upper case flags are negatives (i.e., "don't"s).
+#   Lowercase flags are positives, uppercase flags are negatives ("don't"s).
+#
+#   By default, all phases are enabled, but by specifying a positive phase flag,
+#   all phases become opt-in. If no positive phase flag is specified, a negative
+#   phase flag will disable that phase.
+#
+#   By default, only text documentation is included in the release, but it can
+#   be controlled either by defining a ".release" file in the resource that
+#   overrides the defaults, or via flags. Flags have the highest priority. Using
+#   a positive documentation flag will produce that type of documentation, while
+#   a negative flag will prohibit it (if enabled by default or in ".release").
 #
 #   Args:
 #     <type>      Resource type, one of: "block", "patch", "port", "sprite", "tool", "uberasm"
@@ -30,12 +36,12 @@
 #     -c <hash>   Commit to tag
 #
 #   Phases:
-#     -e          Merge branch
-#     -g          Create tag
-#     -a          Generate archive
-#     -r          Publish release
-#     -d          Notify Discord
-#     -s          Update summary
+#     -e, -E      Merge branch
+#     -g, -G      Create tag
+#     -a, -A      Generate archive
+#     -r, -R      Publish release
+#     -d, -D      Notify Discord
+#     -s, -S      Update summary
 #
 #   Documentation:
 #     -h, -H      Generate HTML documentation
@@ -63,11 +69,17 @@ source .env
 while getopts "eEgGaArRdDsShHtTmMiIc:" flag; do
   case $flag in
     e) FLAG_PHASE_MERGE_TEMP=1    ;;
+    E) FLAG_PHASE_MERGE_TEMP=0    ;;
     g) FLAG_PHASE_TAG_TEMP=1      ;;
+    G) FLAG_PHASE_TAG_TEMP=0      ;;
     a) FLAG_PHASE_ARCHIVE_TEMP=1  ;;
+    A) FLAG_PHASE_ARCHIVE_TEMP=0  ;;
     r) FLAG_PHASE_RELEASE_TEMP=1  ;;
+    R) FLAG_PHASE_RELEASE_TEMP=0  ;;
     d) FLAG_PHASE_DISCORD_TEMP=1  ;;
+    D) FLAG_PHASE_DISCORD_TEMP=0  ;;
     s) FLAG_PHASE_SUMMARY_TEMP=1  ;;
+    S) FLAG_PHASE_SUMMARY_TEMP=0  ;;
     m) FLAG_DOC_MARKDOWN_TEMP=1   ;;
     h) FLAG_DOC_HTML_TEMP=1       ;;
     H) FLAG_DOC_HTML_TEMP=0       ;;
@@ -117,12 +129,12 @@ if [[ -f "./$TYPE_DIR/$NAME/.release" ]]; then
 fi
 
 # All phases are enabled by default, but they become opt-in if any flag is set
-if [[ -n $FLAG_PHASE_MERGE_TEMP ]]   || \
-   [[ -n $FLAG_PHASE_TAG_TEMP ]]     || \
-   [[ -n $FLAG_PHASE_ARCHIVE_TEMP ]] || \
-   [[ -n $FLAG_PHASE_RELEASE_TEMP ]] || \
-   [[ -n $FLAG_PHASE_DISCORD_TEMP ]] || \
-   [[ -n $FLAG_PHASE_SUMMARY_TEMP ]]; then
+if [[ $FLAG_PHASE_MERGE_TEMP == 1 ]]   || \
+   [[ $FLAG_PHASE_TAG_TEMP == 1 ]]     || \
+   [[ $FLAG_PHASE_ARCHIVE_TEMP == 1 ]] || \
+   [[ $FLAG_PHASE_RELEASE_TEMP == 1 ]] || \
+   [[ $FLAG_PHASE_DISCORD_TEMP == 1 ]] || \
+   [[ $FLAG_PHASE_SUMMARY_TEMP == 1 ]]; then
   FLAG_PHASE_DEFAULT=0
 else
   FLAG_PHASE_DEFAULT=1
@@ -184,12 +196,23 @@ OUT_PATH="$OUT_DIR/$OUT_NAME"
 ZIP_NAME="$OUT_NAME.zip"
 ZIP_PATH="$OUT_DIR/$ZIP_NAME"
 
+
+#-------------------------------------------------------------------------------
+# Log
+#-------------------------------------------------------------------------------
+
 # Colors for logging
-C_GOOD='\033[0;32m'
-C_INFO='\033[0;34m'
-C_WARN='\033[0;33m'
-C_FAIL='\033[0;31m'
-C_NONE='\033[0m'
+LOG_COLOR_GOOD='\033[0;32m'
+LOG_COLOR_INFO='\033[0;34m'
+LOG_COLOR_WARN='\033[0;33m'
+LOG_COLOR_FAIL='\033[0;31m'
+LOG_COLOR_NONE='\033[0m'
+
+# Logging functions
+log_good() { printf "${LOG_COLOR_GOOD}$1${LOG_COLOR_NONE}\n"; }
+log_info() { printf "${LOG_COLOR_INFO}$1${LOG_COLOR_NONE}\n"; }
+log_warn() { printf "${LOG_COLOR_WARN}$1${LOG_COLOR_NONE}\n"; }
+log_none() { printf "${LOG_COLOR_FAIL}$1${LOG_COLOR_NONE}\n"; }
 
 
 #-------------------------------------------------------------------------------
@@ -198,19 +221,19 @@ C_NONE='\033[0m'
 
 # Check if resource exists
 if [[ ! -d "$SRC_PATH" ]]; then
-  echo ${C_FAIL}Resource $SRC_PATH doesn\'t exist${C_NONE}
+  log_fail "Resource $SRC_PATH doesn\'t exist"
   exit 1
 fi
 
 # Check if readme exists
 if [[ ! -f "$README_PATH" ]]; then
-  echo ${C_FAIL}Resource $SRC_PATH doesn\'t have a README${C_NONE}
+  log_fail "Resource $SRC_PATH doesn\'t have a README"
   exit 1
 fi
 
 # Check if changelog exists
 if [[ ! -f "$CHANGELOG_PATH" ]]; then
-  echo ${C_FAIL}Resource $SRC_PATH doesn\'t have a CHANGELOG${C_NONE}
+  log_fail "Resource $SRC_PATH doesn\'t have a CHANGELOG"
   exit 1
 fi
 
@@ -225,17 +248,18 @@ fi
 #-------------------------------------------------------------------------------
 
 if [[ $FLAG_PHASE_MERGE != 1 ]]; then
-  echo ${C_WARN}Skip merge: archive only mode${C_NONE}
+  log_warn "Skip merge: disabled"
 elif [[ -n $GIT_HASH ]]; then
-  echo ${C_WARN}Skip merge: already merged${C_NONE}
+  log_warn "Skip merge: already merged"
 elif [[ ! $(git branch --list $GIT_BRANCH) ]]; then
-  echo ${C_WARN}Skip merge: branch $GIT_BRANCH doesn\'t exist${C_NONE}
+  log_warn "Skip merge: branch $GIT_BRANCH doesn\'t exist"
 else
+  log_info "Merge $GIT_BRANCH"
+
   git merge --squash $GIT_BRANCH > /dev/null
   git commit -m $GIT_TAG > /dev/null
   git push > /dev/null
   GIT_HASH=$(git log --all --grep='$GIT_TAG' | grep commit | cut -d\  -f2)
-  echo ${C_INFO}Merge $GIT_BRANCH${C_NONE}
 fi
 
 
@@ -244,15 +268,16 @@ fi
 #-------------------------------------------------------------------------------
 
 if [[ $FLAG_PHASE_TAG != 1 ]]; then
-  echo ${C_WARN}Skip tag: disabled${C_NONE}
+  log_warn "Skip tag: disabled"
 elif [[ -z $GIT_HASH ]]; then
-  echo ${C_WARN}Skip tag: no commit found for $GIT_TAG${C_NONE}
+  log_warn "Skip tag: no commit found for $GIT_TAG"
 elif [[ $(git tag --list $GIT_TAG) ]]; then
-  echo ${C_WARN}Skip tag: tag $GIT_TAG already exists${C_NONE}
+  log_warn "Skip tag: tag $GIT_TAG already exists"
 else
+  log_info "Tag $GIT_TAG"
+
   git tag -a $GIT_TAG $GIT_HASH -m $GIT_TAG > /dev/null
   git push origin $GIT_TAG > /dev/null
-  echo ${C_INFO}Tag $GIT_TAG${C_NONE}
 fi
 
 
@@ -261,15 +286,19 @@ fi
 #-------------------------------------------------------------------------------
 
 if [[ $FLAG_PHASE_ARCHIVE != 1 ]]; then
-  echo ${C_WARN}Skip archive: disabled${C_NONE}
+  log_warn "Skip archive: disabled"
 else
+  log_info "Create archive $ZIP_PATH"
+
   # Copy output directory
   mkdir -p $OUT_DIR
   rm -rf $OUT_PATH $ZIP_PATH
   cp -r $SRC_PATH $OUT_PATH
 
   # Remove images
-  if [[ $FLAG_DOC_IMAGES != 1 ]]; then
+  if [[ $FLAG_DOC_IMAGES == 1 ]]; then
+    log_info "\t...with images"
+  else
     rm -rf $OUT_PATH/docs/assets/images/
     if [[ -d "$OUT_PATH/docs/markdown" ]];
     then sed -i "" -e 's/<img[^>]*>//g' $OUT_PATH/*.md $OUT_PATH/docs/markdown/*.md
@@ -280,15 +309,19 @@ else
   # Generate HTML documentation
   if [[ $FLAG_DOC_HTML == 1 ]]; then
     deno run --allow-read --allow-write $MD2HTML $TYPE $OUT_NAME
+    log_info "\t...with HTML"
   fi
 
   # Generate text documentation (README and CHANGELOG only)
   if [[ $FLAG_DOC_TEXT == 1 ]]; then
     deno run --allow-read --allow-write $MD2TEXT $TYPE $OUT_NAME
+    log_info "\t...with text"
   fi
 
   # Remove markdown documentation
-  if [[ $FLAG_DOC_MARKDOWN != 1 ]]; then
+  if [[ $FLAG_DOC_MARKDOWN == 1 ]]; then
+    log_info "\t...with markdown"
+  else
     rm $OUT_PATH/*.md
     rm -rf $OUT_PATH/docs/markdown/
   fi
@@ -307,9 +340,9 @@ else
   cd - > /dev/null
 
   # Verify that archive has been created
-  if [[ ! -f $ZIP_PATH ]];
-  then echo ${C_FAIL}Failed to create archive${C_NONE}; exit 1
-  else echo ${C_INFO}Create archive $ZIP_PATH${C_NONE}
+  if [[ ! -f $ZIP_PATH ]]; then
+    log_fail "Failed to create archive";
+    exit 1
   fi
 fi
 
@@ -319,19 +352,20 @@ fi
 #-------------------------------------------------------------------------------
 
 if [[ $FLAG_PHASE_RELEASE != 1 ]]; then
-  echo ${C_WARN}Skip release: disabled${C_NONE}
+  log_warn "Skip release: disabled"
 elif [[ $(gh release list | grep $GIT_TAG) ]]; then
-  echo ${C_WARN}Skip release: already released${C_NONE}
+  log_warn "Skip release: already released"
 elif [[ ! $(git tag --list $GIT_TAG) ]]; then
-  echo ${C_WARN}Skip release: tag $GIT_TAG found${C_NONE}
+  log_warn "Skip release: tag $GIT_TAG found"
 else
+  log_info "Publish release $GH_URL"
+
   # Get notes and title
   GH_NOTES=$(deno run --allow-read $GH_GET_NOTES $TYPE $NAME)
   GH_TITLE="$(deno run --allow-read $GH_GET_TITLE $TYPE $NAME) $VERSION"
 
   # Generate release
   GH_URL=$(gh release create $GIT_TAG $ZIP_PATH --latest --notes "$GH_NOTES" --title "$GH_TITLE" --verify-tag)
-  echo ${C_INFO}Publish release $GH_URL${C_NONE}
 fi
 
 
@@ -340,15 +374,16 @@ fi
 #-------------------------------------------------------------------------------
 
 if [[ $FLAG_PHASE_DISCORD != 1 ]]; then
-  echo ${C_WARN}Skip Discord: disabled${C_NONE}
+  log_warn "Skip Discord: disabled"
 elif [[ -z $GH_URL ]]; then
-  echo ${C_WARN}Skip Discord: no release${C_NONE}
+  log_warn "Skip Discord: no release"
 elif [[ -z $DISCORD_WEBHOOK ]]; then
-  echo ${C_WARN}Skip Discord: no webhook${C_NONE}
+  log_warn "Skip Discord: no webhook"
 else
+  log_info "Notify Discord"
+
   GM_EMBED="{\"title\":\"$GH_TITLE\",\"url\":\"$GH_URL\",\"color\":15258703}"
   curl -H "Content-Type: application/json" -d "{\"username\":\"Release\",\"content\":\"\",\"embeds\":[{\"title\":\"$GH_TITLE\",\"url\":\"$GH_URL\"}]}" $DISCORD_WEBHOOK
-  echo ${C_INFO}Notify Discord${C_NONE}
 fi
 
 
@@ -357,12 +392,14 @@ fi
 #-------------------------------------------------------------------------------
 
 if [[ $FLAG_PHASE_SUMMARY != 1 ]]; then
-  echo ${C_WARN}Skip summary: disabled${C_NONE}
+  log_warn "Skip summary: disabled"
 elif [[ -z $(gh release list | grep $GIT_TAG) ]]; then
-  echo ${C_WARN}Skip summary: no release for $GIT_TAG${C_NONE}
+  log_warn "Skip summary: no release for $GIT_TAG"
 elif [[ $(jq ".$TYPE_DIR.$NAME.version" $SUMMARY_JSON) == "\"$VERSION\"" ]]; then
-  echo ${C_WARN}Skip summary: already up to date${C_NONE}
+  log_warn "Skip summary: already up to date"
 else
+  log_info "Update summary"
+
   # Update JSON
   GH_TITLE="$(deno run --allow-read $GH_GET_TITLE $TYPE $NAME)"
   jq ".$TYPE_DIR.$NAME += {\"name\":\"$GH_TITLE\",\"version\":\"$VERSION\"}" $SUMMARY_JSON > $SUMMARY_JSON.temp
@@ -376,9 +413,6 @@ else
   git add $SUMMARY_PATH $SUMMARY_JSON > /dev/null
   git commit -m "Update README" > /dev/null
   git push > /dev/null
-
-  # Notify
-  echo ${C_INFO}Update summary${C_NONE}
 fi
 
 
@@ -387,4 +421,4 @@ fi
 #-------------------------------------------------------------------------------
 
 # If all went well, print a message
-echo ${C_GOOD}Release complete!${C_NONE}
+log_good "Release complete!"
